51e44988dcbf2bb87afa54ccd7aef41e8ccc1ad0
JackPumpkinHead <kostyu444enko.igor@gmail.com>
2023-03-21 16:21:03 +0300

    add font, img and styles

--- index.js
+++ index.js
@@ -7,51 +7,53 @@ function addition() {
     const nc = 1.333;
     const ncml = 0.333;
 
-    var PWM = Number(document.getElementById("PWM").value);
+    let PWM = Number(document.getElementById("PWM").value);
     
-    var PSM = Number(document.getElementById("PSM").value);
+    let PSM = Number(document.getElementById("PSM").value);
     
-    var L = Number(document.getElementById("L").value);
+    let L = Number(document.getElementById("L").value);
 
-    var depth_front_camera = Number(document.getElementById("depth_front_camera").value);
+    let depth_front_camera = Number(document.getElementById("depth_front_camera").value);
 
-    var IOL = Number(document.getElementById("IOL").value);
+    let IOL = Number(document.getElementById("IOL").value);
 
-    var A_not_optimize = Number(document.getElementById("A_not_optimize").value);
+    let A_not_optimize = Number(document.getElementById("A_not_optimize").value);
 
-    var spherical = Number(document.getElementById("spherical").value);
+    let spherical = Number(document.getElementById("spherical").value);
 
-    var cylindrical = Number(document.getElementById("cylindrical").value);
+    let cylindrical = Number(document.getElementById("cylindrical").value);
 
-    var K = (PWM + PSM) / 2;
+    let K = (PWM + PSM) / 2;
 
-    var r = 337.5 / K;
+    let r = 337.5 / K;
 
-    var Lcor = checkLcor(L);
+    let Lcor = checkLcor(L);
 
-    var Cw = -5.41 + 0.58412 * Lcor + 0.098 * K;
+    let Cw = -5.41 + 0.58412 * Lcor + 0.098 * K;
 
-    var H = calcH(r, Cw);
+    let H = calcH(r, Cw);
 
-    var RETHICK = 0.65696 - 0.02029 * L;
+    let RETHICK = 0.65696 - 0.02029 * L;
 
-    var LOPT = L + RETHICK;
+    let LOPT = L + RETHICK;
 
-    var after_operation = spherical + (1/2 * cylindrical);
+    let after_operation = spherical + (1/2 * cylindrical);
 
-    var A_optimize = optimize(after_operation, V, r, ncml, IOL, na, LOPT)
+    let A_optimize = optimize(after_operation, V, r, ncml, IOL, na, LOPT);
+    // var A_optimize = 118.172911872655
     
-    var ACDconst = 0.62467 * A_optimize - 68.747;
+    let ACDconst = 0.62467 * A_optimize - 68.747;
 
-    var Offset = ACDconst - 3.336;
+    let Offset = ACDconst - 3.336;
 
-    var ACDest = H + Offset;
+    let ACDest = H + Offset;
 
-    var REFX = (1000 * na * (na * r - ncml * LOPT) - IOL * (LOPT - ACDest) * (na * r - ncml * ACDest)) / 
+    let REFX = (1000 * na * (na * r - ncml * LOPT) - IOL * (LOPT - ACDest) * (na * r - ncml * ACDest)) /
         (na * (V * (na * r - ncml * LOPT) + LOPT * r) - 0.001 * IOL * (LOPT - ACDest) * (V * (na * r - ncml * ACDest) + ACDest * r));
 
 
-
+    // Начал считать оптимизированную А-константу, но в итоге значение REFX не совпадает с after_operation,
+    // и оптимизированная константа получает значение в 1176 в первом тесте и ~1000 во втором
 
 
     document.getElementById("K").value = K.toFixed(2);
@@ -60,7 +62,7 @@ function addition() {
 
     document.getElementById("Lcor").value = Lcor.toFixed(8);
 
-    document.getElementById("Cw").value = Cw.toFixed(8);    
+    document.getElementById("Cw").value = Cw.toFixed(13);
 
     document.getElementById("H").value = H.toFixed(8);    
 
@@ -81,7 +83,7 @@ function addition() {
     // document.getElementById("IOLemme_first").value = IOLemme_first.toFixed(1);
     // document.getElementById("IOLemme_second").value = IOLemme_second.toFixed(1);
 
-    document.getElementById("REFX").value = REFX.toFixed(2);
+    document.getElementById("REFX").value = REFX.toFixed(3);
 
 
 
@@ -104,27 +106,26 @@ function calcH(r, Cw) {
 function optimize (after_operation, V, r, ncml, IOL, na, LOPT) {
     let FEFX = after_operation;
     let a = - FEFX * ( r - V * ncml) - IOL * ncml;
-
     let b = FEFX * (0.001 * IOL * V * na * r - 0.001 * LOPT * (r - V * ncml)) + IOL * LOPT * ncml + IOL * na * r;
 
-    let c = FEFX * (na * na * V * r - na * V * ncml * LOPT + na * LOPT * r - 0.001 * IOL * LOPT * V * na * r) -
-        1000 * na * na * r + 1000 * na * ncml * LOPT + IOL * LOPT * na * r;
+    let c = FEFX * (Math.pow(na, 2) * V * r - na * V * ncml * LOPT + na * LOPT * r - 0.001 * IOL * LOPT * V * na * r) -
+        1000 * Math.pow(na, 2) * r + 1000 * na * ncml * LOPT + IOL * LOPT * na * r;
 
     return descriminant(a, b, c)
 
 }
 
 function descriminant(a, b, c,) {
-    var D = b * b - 4 * a * c;
+    let D = (Math.pow(b, 2)) - (4 * a * c);
 
-    var x1 = (-b + Math.sqrt(D)) / 2 * a;
+    let x1 = (-b + Math.sqrt(D)) / (2 * a);
 
-    var x2 = (-b - Math.sqrt(D)) / 2 * a;
+    let x2 = (-b - Math.sqrt(D)) / (2 * a);
 
-
-
+    console.log(x1, x2)
     if (x1 >= 0) return x1;
     else return x2;
+
 
 }
 
